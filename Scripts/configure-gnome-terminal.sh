0361d33039543e26c436a91c657aee5b17a890ac
Fabien Engels <fabien.engels@gmail.com>
2012-06-27 09:37:42 +0200

    Update terminal transparency

--- Scripts/configure-gnome-terminal.sh
+++ Scripts/configure-gnome-terminal.sh
@@ -2,7 +2,7 @@
 
 gconftool-2 --set /apps/gnome-terminal/profiles/Default/bold_color_same_as_fg --type bool false
 gconftool-2 --set /apps/gnome-terminal/profiles/Default/use_theme_colors --type bool false
-gconftool-2 --set /apps/gnome-terminal/profiles/Default/background_darkness --type float 0.88
+gconftool-2 --set /apps/gnome-terminal/profiles/Default/background_darkness --type float 0.93
 gconftool-2 --set /apps/gnome-terminal/profiles/Default/background_type --type string "transparent"
 gconftool-2 --set /apps/gnome-terminal/profiles/Default/login_shell --type bool true
 gconftool-2 --set /apps/gnome-terminal/profiles/Default/palette --type string "#070736364242:#DCDC32322F2F:#858599990000:#B5B589890000:#26268B8BD2D2:#D3D336368282:#2A2AA1A19898:#EEEEE8E8D5D5:#00002B2B3636:#CBCB4B4B1616:#58586E6E7575:#65657B7B8383:#838394949696:#6C6C7171C4C4:#9393A1A1A1A1:#FDFDF6F6E3E3"
