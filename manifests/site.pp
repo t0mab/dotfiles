53bd6a105691b6ce521b31554f558c4876f1271a
Fabien Engels <fabien.engels@gmail.com>
2013-01-21 21:34:30 +0100

    Add nasty node

--- manifests/site.pp
+++ manifests/site.pp
@@ -11,6 +11,43 @@ node 'mrtea' {
 		home => $home
 	}
 
+	home::solarized { $user:
+		home => $home
+	}
+
+	home::ssh { $user:
+		home => $home
+	}
+
+	home::tmux { $user:
+		home => $home
+	}
+
+	home::xmodmap { $user:
+		home => $home
+	}
+
+	home::zsh { $user:
+		home => $home
+	}
+
+	git::config { $user:
+		home => $home
+	}
+}
+
+node 'nasty' {
+	packages::common { 'packages': }
+	packages::dev { 'packages': }
+
+	vim::config { $user:
+		home => $home
+	}
+
+	home::solarized { $user:
+		home => $home
+	}
+
 	home::ssh { $user:
 		home => $home
 	}
