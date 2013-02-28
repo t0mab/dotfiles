74bbdaaecea946875ada954ca5e802ab9c16a5c0
Fabien Engels <fabien.engels@gmail.com>
2013-02-28 11:37:26 +0100

    Remove puppet management

--- apply.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/sh
-
-sudo puppet apply --modulepath=modules manifests/site.pp
---
+++ scripts/packages.sh
@@ -0,0 +1,97 @@
+#! /bin/sh
+
+sudo apt-get update -yqq
+
+sudo apt-get dist-upgrade -y
+
+sudo apt-get purge -y\
+	apport\
+	apport-gtk\
+	evolution-data-server\
+	gnome-bluetooth\
+	gwibber\
+	gwibber-service\
+	indicator-messages\
+	mdadm\
+	python-apport\
+	telepathy-indicator\
+	unity-lens-shopping\
+	update-notifier\
+	whoopsie\
+
+sudo apt-get install -y\
+	apt-file\
+	aria2\
+	build-essential\
+	curl\
+	exif\
+	exiv2\
+	exuberant-ctags\
+	fdupes\
+	ghc\
+	git\
+	htop\
+	ipython-notebook\
+	ipython-qtconsole\
+	ipython3\
+	jpegoptim\
+	lftp\
+	libfreetype6-dev\
+	libgdal-dev\
+	libpq-dev\
+	libproj-dev\
+	libxml2-dev\
+	libxslt1-dev\
+	moreutils\
+	ngrep\
+	nmap\
+	octave\
+	postgresql-client\
+	pv\
+	pwgen\
+	pyflakes\
+	pylint\
+	python-dev\
+	python-doc\
+	python-lxml\
+	python-matplotlib\
+	python-numpy\
+	python-pip\
+	python-scipy\
+	python-setuptools\
+	python3-pip\
+	rtmpdump\
+	siege\
+	ssh\
+	sshuttle\
+	texlive-lang-french\
+	texlive-latex-extra\
+	texlive\
+	tidy\
+	tmux\
+	tsung\
+	ufraw-batch\
+	unattended-upgrades\
+	vim\
+	xml-twig-tools\
+	xz-utils\
+	zsh\
+
+[ "$DISPLAY" != ":0" ] && exit
+
+sudo apt-get install -y\
+	chromium-browser\
+	default-jre\
+	gimp\
+	gitg\
+	gitk\
+	gource\
+	indicator-multiload\
+	indicator-weather\
+	inkscape\
+	meld\
+	rdesktop\
+	scrot\
+	ubuntu-restricted-extras\
+	vlc\
+	wallch\
