2003a396ff86d027a010a2c46ea7262a2609c7c4
Fabien Engels <fabien.engels@gmail.com>
2013-03-04 17:42:24 +0100

    Update irssi settings

--- scripts/irc.sh
+++ scripts/irc.sh
@@ -6,8 +6,8 @@ tmux has-session -t $SESSION
 
 if [ $? != 0 ]; then
 	tmux new-session -s $SESSION -d irssi
-	#tmux split-window -v -d "cat ~/.irssi/nicklistfifo"
-	#tmux set-option -t $SESSION status off
+	tmux split-window -h -d -l 20 "cat ~/.irssi/nicklistfifo"
+	tmux set-option -t $SESSION status off
 fi
 
 tmux attach -t $SESSION
