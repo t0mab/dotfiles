4d179bcf03f22ca8018ed64e4ab49ba5d0886bcd
Fabien Engels <fabien.engels@gmail.com>
2013-03-27 09:45:00 +0100

    Remove unused irssi scripts

--- scripts/irc.sh
+++ scripts/irc.sh
@@ -6,7 +6,6 @@ tmux has-session -t $SESSION
 
 if [ $? != 0 ]; then
 	tmux new-session -s $SESSION -d irssi
-	tmux split-window -h -d -l 20 "cat ~/.irssi/nicklistfifo"
 	tmux set-option -t $SESSION status off
 fi
 
