224282781a63d2eaefb0c55307759ac6aef2fab0
Fabien Engels <fabien.engels@gmail.com>
2013-11-07 23:00:21 +0100

    Remove dircolors-solarized submodule

--- submodules.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/sh
-
-git submodule update --init --recursive
-git submodule foreach git pull origin master
-git submodule foreach 'git checkout $(git log --max-count=1 --pretty=format:"%H")'
-git submodule sync
-
-git commit -a -m 'Update submodules'
-git push
