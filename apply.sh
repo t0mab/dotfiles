7e8ab8696f26d94c96e41ce8b40835f598a6f016
Fabien Engels <fabien.engels@gmail.com>
2013-11-12 10:17:03 +0100

    Automatically install salt on Ubuntu

--- apply.sh
+++ apply.sh
@@ -1,3 +1,9 @@
 #! /bin/sh
 
+if [ ! -f /usr/bin/salt-call ]; then
+	sudo apt-get install -y python-software-properties
+	sudo add-apt-repository -y ppa:saltstack/salt
+	sudo apt-get install -y salt-minion
+fi
+
 sudo salt-call state.highstate --local
