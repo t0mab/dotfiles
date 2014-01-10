10f24ffa969be3ec49b162f1e08415774992e3c5
Fabien Engels <fabien.engels@gmail.com>
2014-01-10 14:12:10 +0100

    Add postgis for Mac OS

--- packages/macos/install.sh
+++ packages/macos/install.sh
@@ -1,5 +1,6 @@
 #! /usr/bin/env bash
 
+
 brew install bzr
 brew install ctags
 brew install geos
@@ -15,3 +16,6 @@ brew install proj
 brew install python --with-brewed-openssl
 brew install python3 --with-brewed-openssl
 brew install vim
+
+pip-2.7 install numpy
+brew install postgis
