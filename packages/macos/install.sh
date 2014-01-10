2d4242c82e15622a9478528a322ae7e7bee2d55f
Fabien Engels <fabien.engels@gmail.com>
2014-01-10 09:24:31 +0100

    Fix small issues under Mac OS (backspace and clipboard)

--- packages/macos/install.sh
+++ packages/macos/install.sh
@@ -1,5 +1,17 @@
 #! /usr/bin/env bash
 
+brew install bzr
 brew install ctags
+brew install geos
+brew install gfortran
+brew install git
+brew install hg
+brew install htop-osx
 brew install mongo
+brew install mysql
+brew install nmap
 brew install npm
+brew install proj
+brew install python --with-brewed-openssl
+brew install python3 --with-brewed-openssl
+brew install vim
