7819f72d5caa5db96d76c886d2b5bb0e21789364
Fabien Engels <fabien.engels@gmail.com>
2014-05-15 14:38:54 +0200

    Add YouCompleteMe

--- editors/vim/install.sh
+++ editors/vim/install.sh
@@ -9,18 +9,21 @@ cp -v vimrc ~/.vimrc
 
 cp -v python.py ~/.vim/templates
 
+rsync --recursive --verbose --delete ~/go/misc/vim/ ~/.vim/go
+rsync --recursive --verbose --delete ~/Go/src/github.com/nsf/gocode/vim/ ~/.vim/gocode
+
 git_clone https://github.com/bling/vim-airline.git ~/.vim/airline
 git_clone https://github.com/vim-scripts/closetag.vim.git ~/.vim/closetag
 git_clone https://github.com/kchmck/vim-coffee-script.git ~/.vim/coffeescript
 git_clone https://github.com/tpope/vim-fugitive.git ~/.vim/fugitive
-git_clone https://github.com/Blackrush/vim-gocode.git ~/.vim/gocode
-git_clone https://github.com/jnwhiteh/vim-golang.git ~/.vim/golang
 git_clone https://github.com/vim-scripts/indenthtml.vim.git ~/.vim/indenthtml
-git_clone https://github.com/davidhalter/jedi-vim.git ~/.vim/jedi
 git_clone https://github.com/vim-scripts/matchit.zip.git ~/.vim/matchit
 git_clone https://github.com/altercation/vim-colors-solarized.git ~/.vim/solarized
 git_clone https://github.com/scrooloose/syntastic.git ~/.vim/syntastic
 git_clone https://github.com/majutsushi/tagbar.git ~/.vim/tagbar
 git_clone https://github.com/ervandew/supertab.git ~/.vim/supertab
+git_clone https://github.com/Valloric/YouCompleteMe.git ~/.vim/ycm
+
+cd ~/.vim/ycm && ./install.sh --clang-completer && cd -
 
 sudo pip install --upgrade flake8
