1bff78319121a3477bb23d844df0a61cc0035829
Fabien Engels <fabien.engels@gmail.com>
2014-07-09 15:30:51 +0200

    Improve module clones (skip history and go recursive)

--- helpers.sh
+++ helpers.sh
@@ -3,7 +3,9 @@ function git_clone()
 	url=$1
 	target=$2
 
-	[ ! -d "$target" ] && git clone $url "$target"
+	echo $url
 
-	cd "$target" && git pull && git submodule update --init
+	[ ! -d "$target" ] && git clone --depth 1 $url "$target"
+
+	cd "$target" && git pull && git submodule update --init --recursive
 }
