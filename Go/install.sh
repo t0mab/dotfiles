bb468db706c3eaf384d1362c3f474e05c432892c
Fabien Engels <fabien.engels@gmail.com>
2014-10-13 22:33:17 +0200

    Better Golang support for Vim

--- Go/install.sh
+++ Go/install.sh
@@ -7,10 +7,16 @@ cd ~/go/src && ./all.bash
 
 GOPATH=~/Go ~/go/bin/go get -v -u code.google.com/p/go.tools/cmd/godoc
 GOPATH=~/Go ~/go/bin/go get -v -u code.google.com/p/go.tools/cmd/goimports
+GOPATH=~/Go ~/go/bin/go get -v -u code.google.com/p/go.tools/cmd/gorename
+GOPATH=~/Go ~/go/bin/go get -v -u code.google.com/p/go.tools/cmd/oracle
 GOPATH=~/Go ~/go/bin/go get -v -u code.google.com/p/go.tools/cmd/vet
+GOPATH=~/Go ~/go/bin/go get -v -u code.google.com/p/rog-go/exp/cmd/godef
+GOPATH=~/Go ~/go/bin/go get -v -u github.com/golang/lint/golint
 GOPATH=~/Go ~/go/bin/go get -v -u github.com/jstemmer/gotags
+GOPATH=~/Go ~/go/bin/go get -v -u github.com/jstemmer/gotags
+GOPATH=~/Go ~/go/bin/go get -v -u github.com/kisielk/errcheck
 GOPATH=~/Go ~/go/bin/go get -v -u github.com/lib/pq
 GOPATH=~/Go ~/go/bin/go get -v -u github.com/nsf/gocode
 GOPATH=~/Go ~/go/bin/go get -v -u github.com/streadway/amqp
 GOPATH=~/Go ~/go/bin/go get -v -u github.com/voxelbrain/goptions
-GOPATH=~/Go ~/go/bin/go get -v -u labix.org/v2/mgo
+GOPATH=~/Go ~/go/bin/go get -v -u gopkg.in/mgo.v2
--- Vim/install.sh
+++ Vim/install.sh
@@ -9,20 +9,19 @@ cp -v vimrc ~/.vimrc
 
 cp -v python.py ~/.vim/templates
 
-rsync --recursive --verbose --delete ~/go/misc/vim/ ~/.vim/go
-rsync --recursive --verbose --delete ~/Go/src/github.com/nsf/gocode/vim/ ~/.vim/gocode
+#rsync --recursive --verbose --delete ~/go/misc/vim/ ~/.vim/go
+#rsync --recursive --verbose --delete ~/Go/src/github.com/nsf/gocode/vim/ ~/.vim/gocode
 
+git_clone https://github.com/Valloric/YouCompleteMe.git ~/.vim/ycm
+git_clone https://github.com/altercation/vim-colors-solarized.git ~/.vim/solarized
 git_clone https://github.com/bling/vim-airline.git ~/.vim/airline
-git_clone https://github.com/vim-scripts/closetag.vim.git ~/.vim/closetag
-git_clone https://github.com/kchmck/vim-coffee-script.git ~/.vim/coffeescript
+git_clone https://github.com/ervandew/supertab.git ~/.vim/supertab
+git_clone https://github.com/majutsushi/tagbar.git ~/.vim/tagbar
+git_clone https://github.com/scrooloose/syntastic.git ~/.vim/syntastic
 git_clone https://github.com/tpope/vim-fugitive.git ~/.vim/fugitive
 git_clone https://github.com/vim-scripts/indenthtml.vim.git ~/.vim/indenthtml
 git_clone https://github.com/vim-scripts/matchit.zip.git ~/.vim/matchit
-git_clone https://github.com/altercation/vim-colors-solarized.git ~/.vim/solarized
-git_clone https://github.com/scrooloose/syntastic.git ~/.vim/syntastic
-git_clone https://github.com/majutsushi/tagbar.git ~/.vim/tagbar
-git_clone https://github.com/ervandew/supertab.git ~/.vim/supertab
-git_clone https://github.com/Valloric/YouCompleteMe.git ~/.vim/ycm
+git_clone https://github.com/fatih/vim-go ~/.vim/go
 
 cd ~/.vim/ycm && ./install.sh --clang-completer && cd -
 
