4bd62a1165fda3d334db74ff6314766580f128ef
Fabien Engels <fabien.engels@gmail.com>
2015-01-31 17:30:32 +0100

    Add gitv plugin

--- Vim/install.sh
+++ Vim/install.sh
@@ -2,6 +2,8 @@
 
 . ../helpers.sh
 
+sudo pip install --upgrade flake8
+
 cp -v vimrc ~/.vimrc
 
 [ ! -d ~/.vim-tmp ] && mkdir ~/.vim-tmp
@@ -12,17 +14,19 @@ cp -v python.py ~/.vim/templates
 #rsync --recursive --verbose --delete ~/go/misc/vim/ ~/.vim/go
 #rsync --recursive --verbose --delete ~/Go/src/github.com/nsf/gocode/vim/ ~/.vim/gocode
 
+# To test ...
+# https://github.com/airblade/vim-gitgutter.git
+
 git_clone https://github.com/Valloric/YouCompleteMe.git ~/.vim/ycm
 git_clone https://github.com/altercation/vim-colors-solarized.git ~/.vim/solarized
 git_clone https://github.com/bling/vim-airline.git ~/.vim/airline
 git_clone https://github.com/ervandew/supertab.git ~/.vim/supertab
+git_clone https://github.com/fatih/vim-go ~/.vim/go
+git_clone https://github.com/gregsexton/gitv.git ~/.vim/gitv
 git_clone https://github.com/majutsushi/tagbar.git ~/.vim/tagbar
 git_clone https://github.com/scrooloose/syntastic.git ~/.vim/syntastic
 git_clone https://github.com/tpope/vim-fugitive.git ~/.vim/fugitive
 git_clone https://github.com/vim-scripts/indenthtml.vim.git ~/.vim/indenthtml
 git_clone https://github.com/vim-scripts/matchit.zip.git ~/.vim/matchit
-git_clone https://github.com/fatih/vim-go ~/.vim/go
 
 cd ~/.vim/ycm && ./install.sh --clang-completer && cd -
-
-sudo pip install --upgrade flake8
