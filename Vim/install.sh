9c043cfffdea0094f46db4e98bb5c6c6b3e37f35
Fabien Engels <fabien.engels@gmail.com>
2015-02-05 12:09:52 +0100

    Update vim settings

--- Vim/install.sh
+++ Vim/install.sh
@@ -2,7 +2,7 @@
 
 . ../helpers.sh
 
-sudo pip install --upgrade flake8
+pip_install flake8
 
 cp -v vimrc ~/.vimrc
 
@@ -11,22 +11,29 @@ cp -v vimrc ~/.vimrc
 
 cp -v python.py ~/.vim/templates
 
-#rsync --recursive --verbose --delete ~/go/misc/vim/ ~/.vim/go
-#rsync --recursive --verbose --delete ~/Go/src/github.com/nsf/gocode/vim/ ~/.vim/gocode
-
-# To test ...
-# https://github.com/airblade/vim-gitgutter.git
-
-git_clone https://github.com/Valloric/YouCompleteMe.git ~/.vim/ycm
-git_clone https://github.com/altercation/vim-colors-solarized.git ~/.vim/solarized
-git_clone https://github.com/bling/vim-airline.git ~/.vim/airline
+# Completion
+git_clone https://github.com/Shougo/neocomplete.vim.git ~/.vim/neocomplete
 git_clone https://github.com/ervandew/supertab.git ~/.vim/supertab
-git_clone https://github.com/fatih/vim-go ~/.vim/go
-git_clone https://github.com/gregsexton/gitv.git ~/.vim/gitv
+
+# Colors
+git_clone https://github.com/altercation/vim-colors-solarized.git ~/.vim/solarized
+git_clone https://github.com/tomasr/molokai.git ~/.vim/molokai
+
+# Interface
+git_clone https://github.com/bling/vim-airline.git ~/.vim/airline
 git_clone https://github.com/majutsushi/tagbar.git ~/.vim/tagbar
-git_clone https://github.com/scrooloose/syntastic.git ~/.vim/syntastic
-git_clone https://github.com/tpope/vim-fugitive.git ~/.vim/fugitive
-git_clone https://github.com/vim-scripts/indenthtml.vim.git ~/.vim/indenthtml
 git_clone https://github.com/vim-scripts/matchit.zip.git ~/.vim/matchit
 
-cd ~/.vim/ycm && ./install.sh --clang-completer && cd -
+# Git
+git_clone https://github.com/airblade/vim-gitgutter.git ~/.vim/gitgutter
+git_clone https://github.com/gregsexton/gitv.git ~/.vim/gitv
+git_clone https://github.com/tpope/vim-fugitive.git ~/.vim/fugitive
+
+# Go
+git_clone https://github.com/fatih/vim-go ~/.vim/go
+
+# Lint
+git_clone https://github.com/scrooloose/syntastic.git ~/.vim/syntastic
+
+# HTML
+git_clone https://github.com/vim-scripts/indenthtml.vim.git ~/.vim/indenthtml
