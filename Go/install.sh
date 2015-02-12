5c8b5a88a7550215121aa989a219aba387c04ede
Fabien Engels <fabien.engels@gmail.com>
2015-02-12 14:39:46 +0100

    Update Go installation

--- Go/install.sh
+++ Go/install.sh
@@ -1,22 +1,24 @@
-[ ! -d ~/go/.hg ] && hg clone -u release https://code.google.com/p/go ~/go
+#! /bin/bash
 
-cd ~/go && hg pull && hg update release
+set -e
+
+[ ! -d ~/go/.git ] && git clone https://go.googlesource.com/go ~/go
+
+cd ~/go && git fetch && git checkout go1.4.1
 cd ~/go/src && ./all.bash
 
 [ ! -d ~/Go ] && mkdir ~/Go
 
-GOPATH=~/Go ~/go/bin/go get -v -u code.google.com/p/go.tools/cmd/godoc
 GOPATH=~/Go ~/go/bin/go get -v -u code.google.com/p/go.tools/cmd/goimports
-GOPATH=~/Go ~/go/bin/go get -v -u code.google.com/p/go.tools/cmd/gorename
 GOPATH=~/Go ~/go/bin/go get -v -u code.google.com/p/go.tools/cmd/oracle
-GOPATH=~/Go ~/go/bin/go get -v -u code.google.com/p/go.tools/cmd/vet
 GOPATH=~/Go ~/go/bin/go get -v -u code.google.com/p/rog-go/exp/cmd/godef
 GOPATH=~/Go ~/go/bin/go get -v -u github.com/golang/lint/golint
 GOPATH=~/Go ~/go/bin/go get -v -u github.com/jstemmer/gotags
-GOPATH=~/Go ~/go/bin/go get -v -u github.com/jstemmer/gotags
 GOPATH=~/Go ~/go/bin/go get -v -u github.com/kisielk/errcheck
 GOPATH=~/Go ~/go/bin/go get -v -u github.com/lib/pq
-GOPATH=~/Go ~/go/bin/go get -v -u github.com/nsf/gocode
 GOPATH=~/Go ~/go/bin/go get -v -u github.com/streadway/amqp
 GOPATH=~/Go ~/go/bin/go get -v -u github.com/voxelbrain/goptions
+GOPATH=~/Go ~/go/bin/go get -v -u golang.org/x/tools/cmd/godoc
+GOPATH=~/Go ~/go/bin/go get -v -u golang.org/x/tools/cmd/gorename
+GOPATH=~/Go ~/go/bin/go get -v -u golang.org/x/tools/cmd/vet
 GOPATH=~/Go ~/go/bin/go get -v -u gopkg.in/mgo.v2
