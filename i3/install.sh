e3dcef7d6f693a6a8b3ab9cde20e49aa3581e421
Fabien Engels <fabien.engels@gmail.com>
2015-03-02 14:55:06 +0100

    Add light solarized theme for i3

--- i3/install.sh
+++ i3/install.sh
@@ -13,3 +13,4 @@ cp -v i3status_config ~/.config/i3status/config
 cp -v xbindkeysrc ~/.xbindkeysrc
 cp -v xinitrc ~/.xinitrc
 cp -v Xresources ~/.Xresources
+cp -v Xresources-light ~/.Xresources-light
