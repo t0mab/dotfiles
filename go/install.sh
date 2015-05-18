73a2f520d291abddea7f24a156a9df601ac6f313
Fabien Engels <fabien.engels@gmail.com>
2015-05-18 15:48:26 +0200

    Update Go packages list

--- go/install.sh
+++ go/install.sh
@@ -9,8 +9,6 @@ cd ~/go/src && ./all.bash
 
 [ ! -d ~/Go ] && mkdir ~/Go
 
-GOPATH=~/Go ~/go/bin/go get -v -u code.google.com/p/go.tools/cmd/goimports
-GOPATH=~/Go ~/go/bin/go get -v -u code.google.com/p/go.tools/cmd/oracle
 GOPATH=~/Go ~/go/bin/go get -v -u code.google.com/p/rog-go/exp/cmd/godef
 GOPATH=~/Go ~/go/bin/go get -v -u github.com/coreos/etcd-ca
 GOPATH=~/Go ~/go/bin/go get -v -u github.com/golang/lint/golint
@@ -18,10 +16,18 @@ GOPATH=~/Go ~/go/bin/go get -v -u github.com/jstemmer/gotags
 GOPATH=~/Go ~/go/bin/go get -v -u github.com/kisielk/errcheck
 GOPATH=~/Go ~/go/bin/go get -v -u github.com/lib/pq
 GOPATH=~/Go ~/go/bin/go get -v -u github.com/mrjones/oauth
+GOPATH=~/Go ~/go/bin/go get -v -u github.com/schachmat/wego
+GOPATH=~/Go ~/go/bin/go get -v -u github.com/spf13/hugo
 GOPATH=~/Go ~/go/bin/go get -v -u github.com/streadway/amqp
 GOPATH=~/Go ~/go/bin/go get -v -u github.com/voxelbrain/goptions
 GOPATH=~/Go ~/go/bin/go get -v -u golang.org/x/tools/cmd/benchcmp
 GOPATH=~/Go ~/go/bin/go get -v -u golang.org/x/tools/cmd/godoc
+GOPATH=~/Go ~/go/bin/go get -v -u golang.org/x/tools/cmd/goimports
 GOPATH=~/Go ~/go/bin/go get -v -u golang.org/x/tools/cmd/gorename
 GOPATH=~/Go ~/go/bin/go get -v -u golang.org/x/tools/cmd/vet
+GOPATH=~/Go ~/go/bin/go get -v -u golang.org/x/tools/oracle
 GOPATH=~/Go ~/go/bin/go get -v -u gopkg.in/mgo.v2
+
+# Go go Vegeta !
+GOPATH=~/Go ~/go/bin/go get -v -u github.com/tsenart/vegeta
+GOPATH=~/Go ~/go/bin/go install -v github.com/tsenart/vegeta
