4fd8839bb94e3d446c82e05bda9447e6034cbe41
Fabien Engels <fabien.engels@gmail.com>
2015-06-18 12:10:09 +0200

    Add missing MPD folders

--- music/install.sh
+++ music/install.sh
@@ -4,6 +4,8 @@ set -e
 
 mkdir -p ~/.config/beets
 mkdir -p ~/.ncmpcpp
+mkdir -p ~/.config/mpd/logs
+mkdir -p ~/.config/mpd/playlists
 
 cp -v beets.yaml ~/.config/beets/config.yaml
 cp -v mpd.conf ~/.mpdconf
