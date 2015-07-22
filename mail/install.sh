7ea6ab0dffbc69f68e8f7c3471c4d8d343e9beb2
Fabien Engels <fabien.engels@gmail.com>
2015-07-22 15:20:29 +0200

    Add missing cp (notmuchconfig)

--- mail/install.sh
+++ mail/install.sh
@@ -17,3 +17,4 @@ cp -v muttrc ~/.muttrc
 cp -v mutt-colors-solarized-dark-16.muttrc ~/.mutt/mutt-colors-solarized-dark-16.muttrc
 cp -v signature ~/.mutt/signature
 cp -v mailcap ~/.mutt/mailcap
+cp -v notmuch-config ~/.notmuch-config
