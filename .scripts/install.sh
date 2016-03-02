16b08cceeace730d78b62dec18403e8fc96c85dd
Fabien Engels <fabien.engels@gmail.com>
2016-03-02 10:08:52 +0100

    Add install.sh

--- .scripts/install.sh
+++ .scripts/install.sh
@@ -2,8 +2,63 @@
 
 set -e
 
-# sudo pacman -Syu && sudo pacman --needed --noconfirm -S \
-# 	go
+echo "Installing and upgrading packages ..."
+sudo pacman -Syu && sudo pacman --needed --noconfirm -S \
+	abook \
+	aria2 \
+	beets \
+	curl \
+	dfc \
+	dos2unix \
+	fdupes \
+	git \
+	go \
+	i3-wm \
+	i3lock \
+	i3status \
+	imagemagick \
+	ipcalc \
+	isync \
+	jhead \
+	jq \
+	lftp \
+	mpc \
+	mpd \
+	msmtp \
+	mtr \
+	mutt \
+	ncdu \
+	ncmpcpp \
+	neovim \
+	nmap \
+	notmuch \
+	p7zip \
+	parallel \
+	pass \
+	postgresql \
+	pv \
+	ranger \
+	redshift \
+	screenfetch \
+	scrot \
+	siege \
+	slim \
+	syncthing \
+	termite \
+	testdisk \
+	tmux \
+	unrar \
+	wget \
+	xbindkeys \
+	youtube-dl \
+	zsh \
+
+echo "Enable and start syncthing ..."
+sudo systemctl enable syncthing@fabien
+sudo systemctl start syncthing@fabien
+
+echo "Updating fonts cache ..."
+fc-cache -f ~/.local/share/fonts
 
 export GOPATH="$HOME/Go"
 
@@ -14,6 +69,7 @@ GO_REPOS="\
 	github.com/lib/pq \
 	github.com/nsf/gocode \
 	github.com/rogpeppe/godef \
+	github.com/tsenart/vegeta \
 	golang.org/x/tools/cmd/benchcmp \
 	golang.org/x/tools/cmd/goimports \
 	golang.org/x/tools/cmd/gorename \
@@ -21,7 +77,12 @@ GO_REPOS="\
 	golang.org/x/tools/oracle \
 "
 
+# GO_REPOS=""
+
 for repos in $GO_REPOS; do
 	echo "Installing $repos ..."
 	go get -u $repos
 done
+
+echo "Install vegeta binary ..."
+go install github.com/tsenart/vegeta
