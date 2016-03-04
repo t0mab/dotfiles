84330c8c65748b0c2dc9049e1cb0e1eb2bc96766
Fabien Engels <fabien.engels@gmail.com>
2016-03-04 17:06:21 +0100

    Update packages list

--- .scripts/install.sh
+++ .scripts/install.sh
@@ -5,58 +5,108 @@ set -e
 echo "Installing and upgrading packages ..."
 sudo pacman -Syu && sudo pacman --needed --noconfirm -S \
 	abook \
+	alsa-firmware \
+	alsa-plugins \
+	alsa-utils \
 	aria2 \
 	beets \
+	bind-tools \
+	brasero \
+	chromium \
 	compton \
+	cups \
 	curl \
 	darktable \
 	dfc \
 	dos2unix \
+	dosfstools \
+	dstat \
+	evince \
+	exfat-utils \
 	fdupes \
+	feh \
 	firefox \
+	fuse-exfat \
+	gcc \
 	gimp \
 	git \
 	go \
+	gparted \
+	gpicview \
+	gptfdisk \
+	hddtemp \
+	htop \
+	httpie \
 	i3-wm \
 	i3lock \
 	i3status \
 	imagemagick \
 	inkscape \
+	iotop \
 	ipcalc \
 	isync \
 	jhead \
 	jq \
 	lftp \
+	libcups \
+	libreoffice \
+	lsof \
+	lxappearance \
+	lynx \
+	mesa-demos \
+	moreutils \
 	mpc \
 	mpd \
+	mpv \
 	msmtp \
 	mtr \
 	mutt \
 	ncdu \
 	ncmpcpp \
 	neovim \
+	netctl \
+	nethogs \
 	nmap \
 	notmuch \
+	ntfs-3g \
+	openssh \
+	openvpn \
 	p7zip \
 	parallel \
 	pass \
+	pkgfile \
 	postgresql \
+	pulseaudio \
+	pulseaudio-alsa \
 	pv \
+	python-pip \
+	python-virtualenvwrapper \
 	ranger \
 	redshift \
+	rsync \
 	screenfetch \
 	scrot \
 	siege \
 	slim \
+	strace \
 	syncthing \
 	termite \
 	testdisk \
 	tmux \
+	traceroute \
+	transmission-gtk \
+	ufw \
 	unrar \
 	wget \
 	xbindkeys \
+	xorg-server \
+	xorg-server-utils \
+	xorg-xinit \
 	youtube-dl \
 	zsh \
+
+echo "Installing python packages ..."
+pip install --user --upgrade neovim
 
 echo "Enable and start syncthing ..."
 sudo systemctl enable syncthing@fabien
