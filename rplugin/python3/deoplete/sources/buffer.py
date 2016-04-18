fcceb1f97de908b56dd8f5243e7fb4f482cd20ef
Fabien Engels <fabien.engels@gmail.com>
2016-04-18 09:42:08 +0200

    Squashed '.config/nvim/bundle/deoplete/' changes from 51e7af8..998c411
    998c411 Add on_buffer()
    c5465c9 Fix #252 disable check_cache() by the flick
    0669f01 Check buflisted
    bad1a3d Fix #247 complete from other buffers
    8a4b963 Improve make cache behavior in buffer source
    a037bbd Fix max width behavior
    54445ec Fix #248 add max_abbr_width and max_menu_width
    8677dfb Improve the documentation
    1f13fce Fix #249 documentation
    df9b631 Fix #227 provide disable/enable/toggle functions
    7540fa9 Add external source
    
    git-subtree-dir: .config/nvim/bundle/deoplete
    git-subtree-split: 998c411e9b9f9c2992f4796bcd220bbf66a7a174

--- rplugin/python3/deoplete/sources/buffer.py
+++ rplugin/python3/deoplete/sources/buffer.py
@@ -21,26 +21,7 @@ class Source(Base):
         self.__max_lines = 5000
 
     def gather_candidates(self, context):
-        p = re.compile(context['keyword_patterns'])
-
-        try:
-            if (self.vim.current.buffer.number in self.__buffers) and len(
-                    self.vim.current.buffer) > self.__max_lines:
-                line = context['position'][1]
-                self.__buffers[self.vim.current.buffer.number][
-                    'candidates'] += functools.reduce(operator.add, [
-                        p.findall(x) for x in self.vim.current.buffer[
-                            max([0, line - 500]): line + 500]
-                    ])
-            else:
-                self.__buffers[self.vim.current.buffer.number] = {
-                    'filetype': context['filetype'],
-                    'candidates': functools.reduce(operator.add, [
-                        p.findall(x) for x in self.vim.current.buffer
-                    ]),
-                }
-        except UnicodeDecodeError:
-            return []
+        self.__make_cache(context, self.vim.current.buffer.number)
 
         buffers = [x['candidates'] for x in self.__buffers.values()
                    if x['filetype'] in context['filetypes']]
@@ -49,3 +30,32 @@ class Source(Base):
 
         return [{'word': x} for x in
                 functools.reduce(operator.add, buffers)]
+
+    def on_buffer(self, context):
+        if (self.vim.current.buffer.number
+                not in self.__buffers and
+                self.vim.current.buffer.options['modifiable']):
+            self.__make_cache(context, self.vim.current.buffer.number)
+
+    def __make_cache(self, context, bufnr):
+        p = re.compile(context['keyword_patterns'])
+
+        bufnr -= 1
+        try:
+            if (bufnr in self.__buffers) and len(
+                    self.vim.buffers[bufnr]) > self.__max_lines:
+                line = context['position'][1]
+                self.__buffers[bufnr][
+                    'candidates'] += functools.reduce(operator.add, [
+                        p.findall(x) for x in self.vim.buffers[bufnr][
+                            max([0, line - 500]): line + 500]
+                    ])
+            else:
+                self.__buffers[bufnr] = {
+                    'filetype': context['filetype'],
+                    'candidates': functools.reduce(operator.add, [
+                        p.findall(x) for x in self.vim.buffers[bufnr]
+                    ]),
+                }
+        except UnicodeDecodeError:
+            return []
